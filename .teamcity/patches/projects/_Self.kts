b8d8af4330a7d3eb6470042ec36761a1b9065640
admin <admin@TeamCity>
2024-09-16 12:47:52 +0200

    TeamCity change in 'Kubernetes Example' project: Connection updated

--- .teamcity/patches/projects/_Self.kts
+++ .teamcity/patches/projects/_Self.kts
@@ -41,7 +41,7 @@ changeProject(DslContext.projectId) {
                 name = "HashiCorp Vault"
                 authMethod = appRole {
                     roleId = "f3e75c6b-118b-48a1-97fc-6b8a69eb3bc3"
-                    secretId = "credentialsJSON:ae591de4-9da6-4600-85e6-9cc5b4710453"
+                    secretId = "credentialsJSON:26e2160e-39b8-4d6c-af12-20913b9eaa40"
                 }
                 failOnError = false
             }
