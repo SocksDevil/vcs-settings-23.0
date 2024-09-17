787e846a5a42181903bd676e7f7504fa209bc198
admin <admin@TeamCity>
2024-09-17 12:40:34 +0200

    TeamCity change in 'Kubernetes Example' project: Connection updated

--- .teamcity/patches/projects/_Self.kts
+++ .teamcity/patches/projects/_Self.kts
@@ -63,7 +63,7 @@ changeProject(DslContext.projectId) {
             hashiCorpVaultConnection {
                 id = "hashicorpVaultConnection1"
                 name = "HashiCorp Vault"
-                url = "https://cuddly-ears-win.loca.lt"
+                url = "http://127.0.0.1:8200"
                 authMethod = appRole {
                     roleId = "f3e75c6b-118b-48a1-97fc-6b8a69eb3bc3"
                     secretId = "credentialsJSON:a90c9e97-f71b-4571-bfff-24f0eb0db85b"
