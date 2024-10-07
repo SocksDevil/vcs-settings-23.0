58d95a6b5248829740aae9fde26300708c06473e
admin <admin@TeamCity>
2024-10-07 15:41:41 +0200

    TeamCity change in 'Kubernetes Example' project: Executor PROJECT_EXT_4 of type KubernetesExecutor was updated

--- .teamcity/_Self/Project.kt
+++ .teamcity/_Self/Project.kt
@@ -82,7 +82,7 @@ object Project : Project({
         }
         kubernetesExecutor {
             id = "PROJECT_EXT_4"
-            connectionId = "PROJECT_EXT_16"
+            connectionId = "PROJECT_EXT_10"
             profileName = "Meow"
             templateContainer = """
                 apiVersion: v1
@@ -94,7 +94,6 @@ object Project : Project({
                       # To modify the volume used to run the build, add a volume with the name agent-volume
                     - name: template-container
             """.trimIndent()
-            enabled = true
             param("profileServerUrl", "https://teamcity.evielution.me/bs")
             param("system.cloud.profile_id", "PROJECT_EXT_4")
         }
