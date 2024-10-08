eee8c60c7b64a1dd990fb83a6e7b26fb292232a4
admin <admin@TeamCity>
2024-10-08 10:40:40 +0200

    TeamCity change in 'Kubernetes Example' project: runners of 'this is supposed to be a very long name to see if I can break stuff uwu slay' build configuration were updated

--- .teamcity/_Self/buildTypes/ThisIsSupposedToBeAVeryLongNameToSeeIfICanBreakStuffUwuSlay.kt
+++ .teamcity/_Self/buildTypes/ThisIsSupposedToBeAVeryLongNameToSeeIfICanBreakStuffUwuSlay.kt
@@ -1,7 +1,6 @@
 package _Self.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
@@ -13,11 +12,6 @@ object ThisIsSupposedToBeAVeryLongNameToSeeIfICanBreakStuffUwuSlay : BuildType({
     }
 
     steps {
-        gradle {
-            id = "gradle_runner"
-            tasks = "clean build"
-            gradleWrapperPath = ""
-        }
         maven {
             id = "Maven2"
             goals = "clean test"
