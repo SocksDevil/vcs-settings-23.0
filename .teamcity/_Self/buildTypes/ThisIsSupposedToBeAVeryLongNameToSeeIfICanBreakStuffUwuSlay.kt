8c6909c5e7f9b1741760fa4250cf2fe6ffe9cfa8
admin <admin@TeamCity>
2024-10-09 17:04:09 +0200

    TeamCity change in 'Kubernetes Example' project: VCS roots of 'this is supposed to be a very long name to see if I can break stuff uwu slay' build configuration were updated

--- .teamcity/_Self/buildTypes/ThisIsSupposedToBeAVeryLongNameToSeeIfICanBreakStuffUwuSlay.kt
+++ .teamcity/_Self/buildTypes/ThisIsSupposedToBeAVeryLongNameToSeeIfICanBreakStuffUwuSlay.kt
@@ -8,6 +8,10 @@ import jetbrains.buildServer.configs.kotlin.triggers.vcs
 object ThisIsSupposedToBeAVeryLongNameToSeeIfICanBreakStuffUwuSlay : BuildType({
     name = "this is supposed to be a very long name to see if I can break stuff uwu slay"
 
+    vcs {
+        root(_Self.vcsRoots.HttpsGithubComSocksDevilTeamcityAwsLambdaPluginExampleRefsHeadsMain)
+    }
+
     steps {
         maven {
             id = "Maven2"
