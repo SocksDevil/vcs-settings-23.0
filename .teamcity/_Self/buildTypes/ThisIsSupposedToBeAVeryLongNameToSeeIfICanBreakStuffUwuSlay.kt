ad7ed4a9620fead9d72aacf391704820b0f71a0c
admin <admin@TeamCity>
2024-10-09 17:27:26 +0200

    TeamCity change in 'Kubernetes Example' project: runners of 'this is supposed to be a very long name to see if I can break stuff uwu slay' build configuration were updated

--- .teamcity/_Self/buildTypes/ThisIsSupposedToBeAVeryLongNameToSeeIfICanBreakStuffUwuSlay.kt
+++ .teamcity/_Self/buildTypes/ThisIsSupposedToBeAVeryLongNameToSeeIfICanBreakStuffUwuSlay.kt
@@ -1,8 +1,8 @@
 package _Self.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.dotnetBuild
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
-import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
 object ThisIsSupposedToBeAVeryLongNameToSeeIfICanBreakStuffUwuSlay : BuildType({
@@ -19,10 +19,10 @@ object ThisIsSupposedToBeAVeryLongNameToSeeIfICanBreakStuffUwuSlay : BuildType({
             goals = "clean test"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
         }
-        script {
+        dotnetBuild {
             name = "dotnettest"
             id = "dotnettest"
-            scriptContent = "sleep 100000"
+            runtime = "dotnet"
             param("teamcity.kubernetes.executor.container.image", "mcr.microsoft.com/dotnet/sdk:8.0")
             param("teamcity.kubernetes.executor.pull.policy", "IfNotPresent")
         }
