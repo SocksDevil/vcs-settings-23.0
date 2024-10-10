61ca7d63675fbbbffafc6e8efbbf9c3184c883d0
admin <admin@TeamCity>
2024-10-10 14:37:46 +0200

    TeamCity change in 'Kubernetes Example' project: build features of 'Build - Parallel Tests' build configuration were updated

--- .teamcity/_Self/buildTypes/BuildParallelTests.kt
+++ .teamcity/_Self/buildTypes/BuildParallelTests.kt
@@ -1,6 +1,7 @@
 package _Self.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.parallelTests
 import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
@@ -21,6 +22,12 @@ object BuildParallelTests : BuildType({
 
     triggers {
         vcs {
+        }
+    }
+
+    features {
+        parallelTests {
+            numberOfBatches = 2
         }
     }
 })
