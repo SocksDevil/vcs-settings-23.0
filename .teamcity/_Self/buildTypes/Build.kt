5d54971dd97da90aa1fd19db355adf98c3afb8ac
admin <admin@TeamCity>
2024-11-25 13:40:58 +0100

    TeamCity change in 'Kubernetes Example' project: Synchronization with own VCS root is enabled

---
+++ .teamcity/_Self/buildTypes/Build.kt
@@ -0,0 +1,50 @@
+package _Self.buildTypes
+
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
+
+object Build : BuildType({
+    name = "Build"
+
+    params {
+        param("TEST", "")
+        param("vaultParam", "meow")
+        param("meow", "meowmeow")
+    }
+
+    steps {
+        script {
+            id = "simpleRunner_1"
+            scriptContent = """
+                echo "##teamcity[setParameter name='TEST' value='test']"
+                echo '%vaultParam%' > meow.txt
+                sleep 1000000
+            """.trimIndent()
+        }
+        gradle {
+            id = "gradle_runner"
+            tasks = "clean build"
+            gradleWrapperPath = ""
+        }
+        script {
+            id = "simpleRunner"
+            scriptContent = """
+                echo '%vaultParam%' >> meow.txt
+                echo '%TEST%' >> meow.txt
+                #sleep 1000000
+            """.trimIndent()
+            param("teamcity.kubernetes.executor.pull.policy", "IfNotPresent")
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+
+    failureConditions {
+        executionTimeoutMin = 1
+    }
+})
