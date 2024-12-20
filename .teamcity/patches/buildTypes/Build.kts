9a991487e85c71ff6a0766800f45e4e15161d3ed
admin <admin@TeamCity>
2024-12-20 15:11:43 +0100

    TeamCity change in 'Kubernetes Example' project: runners of 'Build' build configuration were updated

--- .teamcity/patches/buildTypes/Build.kts
+++ .teamcity/patches/buildTypes/Build.kts
@@ -2,6 +2,8 @@ package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.swabra
+import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.ui.*
 
 /*
@@ -10,6 +12,40 @@ To apply the patch, change the buildType with id = 'Build'
 accordingly, and delete the patch script.
 */
 changeBuildType(RelativeId("Build")) {
+    expectSteps {
+        script {
+            id = "simpleRunner_1"
+            scriptContent = """
+                echo "##teamcity[setParameter name='TEST' value='test']"
+                echo '%vaultParam%' > meow.txt
+            """.trimIndent()
+        }
+        gradle {
+            id = "gradle_runner"
+            tasks = "clean build"
+            gradleWrapperPath = ""
+        }
+        script {
+            id = "simpleRunner"
+            scriptContent = """
+                echo '%vaultParam%' >> meow.txt
+                echo '%TEST%' >> meow.txt
+                #sleep 1000000
+            """.trimIndent()
+            param("teamcity.kubernetes.executor.pull.policy", "IfNotPresent")
+        }
+    }
+    steps {
+        insert(0) {
+            gradle {
+                id = "gradle_runner"
+                tasks = "clean build"
+                gradleWrapperPath = ""
+            }
+        }
+        items.removeAt(2)
+    }
+
     features {
         add {
             swabra {
