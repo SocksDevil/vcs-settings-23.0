5f31317244cc32c2b0edc6d5abf81df77e00dbc1
admin <admin@TeamCity>
2025-09-25 11:53:01 +0200

    Synchronization with own VCS root is enabled (TeamCity change in 'Kubernetes Example' project)

---
+++ .teamcity/_Self/vcsRoots/HttpsGithubComSocksDevilTeamcityAwsLambdaPluginExampleRefsHeadsMain1.kt
@@ -0,0 +1,15 @@
+package _Self.vcsRoots
+
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
+
+object HttpsGithubComSocksDevilTeamcityAwsLambdaPluginExampleRefsHeadsMain1 : GitVcsRoot({
+    name = "https://github.com/SocksDevil/teamcity-aws-lambda-plugin-example#refs/heads/main (1)"
+    url = "https://github.com/SocksDevil/teamcity-aws-lambda-plugin-example"
+    branch = "refs/heads/main"
+    branchSpec = "refs/heads/*"
+    authMethod = password {
+        userName = "SocksDevil"
+        password = "credentialsJSON:2091df62-b0a6-494c-9dd1-b03db1aaf9c6"
+    }
+})
