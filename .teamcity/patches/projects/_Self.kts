2c67b1882f70f9844d7995fca0fb417d2b0cc3f1
admin <admin@TeamCity>
2025-10-23 11:34:47 +0200

    Cloud profile 'kube-5' in project 'project1' is now enabled (TeamCity change in 'Kubernetes Example' project)

--- .teamcity/patches/projects/_Self.kts
+++ .teamcity/patches/projects/_Self.kts
@@ -78,7 +78,6 @@ changeProject(DslContext.projectId) {
             }
         }
         feature4.apply {
-            enabled = false
             name = "K8S Test"
             terminateAfterBuild = true
             terminateIdleMinutes = 30
