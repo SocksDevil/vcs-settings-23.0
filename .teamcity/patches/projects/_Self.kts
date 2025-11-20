2aa9af43374bf6b40d64ecdb6f53475a5d3ec072
admin <admin@TeamCity>
2025-11-20 12:59:52 +0100

    Updated cloud profile 'kube-5' in project 'project1' (TeamCity change in 'Kubernetes Example' project)

--- .teamcity/patches/projects/_Self.kts
+++ .teamcity/patches/projects/_Self.kts
@@ -75,13 +75,14 @@ changeProject(DslContext.projectId) {
             }
         }
         feature3.apply {
+            enabled = false
             name = "K8S Test"
             terminateAfterBuild = true
             terminateIdleMinutes = 30
             apiServerURL = "https://6c60846089ad8c095bed3b18ff6d84a0.gr7.eu-west-1.eks.amazonaws.com"
             caCertData = "credentialsJSON:c77bc0a7-f461-4ca8-959b-ee5c8f6389b6"
             namespace = "executor-pods"
-            param("proxyServer", "http://127.0.0.1:8080")
+            param("proxyServer", "http://127.0.0.1:8090")
         }
         val feature4 = find<KubernetesCloudProfile> {
             kubernetesCloudProfile {
