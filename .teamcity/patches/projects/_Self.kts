95f6df20ada0fb3839b287d23aae4e80e6eda1ad
admin <admin@TeamCity>
2026-01-14 11:31:49 +0100

    Executor PROJECT_EXT_30 of type KubernetesExecutor was updated (TeamCity change in 'Kubernetes Example' project)

--- .teamcity/patches/projects/_Self.kts
+++ .teamcity/patches/projects/_Self.kts
@@ -24,6 +24,7 @@ changeProject(DslContext.projectId) {
             }
         }
         feature1.apply {
+            templateName = "pod-test"
             param("enabled", "")
         }
         val feature2 = find<KubernetesCloudProfile> {
