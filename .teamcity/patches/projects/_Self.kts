353fa726aff53f7f982ffa236866a0967bad9930
admin <admin@TeamCity>
2026-01-27 11:18:38 +0100

    Updated cloud profile 'kube-5' in project 'project1' (TeamCity change in 'Kubernetes Example' project)

--- .teamcity/patches/projects/_Self.kts
+++ .teamcity/patches/projects/_Self.kts
@@ -80,10 +80,10 @@ changeProject(DslContext.projectId) {
         }
         add {
             kubernetesCloudImage {
-                id = "PROJECT_EXT_55"
+                id = "PROJECT_EXT_56"
                 profileId = "kube-5"
                 agentPoolId = "-2"
-                agentNamePrefix = "linuxlinuxlinuxlinuxlinuxlinuxlinuxlinuxlinuxlinuxlinuxlinux1"
+                agentNamePrefix = "linux"
                 podSpecification = runContainer {
                     dockerImage = "jetbrains/teamcity-agent:2025.07"
                 }
